f854d44416c53504a09c7b03a3dcde6294449fdb
Ryan Warrener <ryan.warrener@ontariotechu.net>
2026-04-06 15:24:38 -0400

    Test commit

--- gathering/macos/Flutter/GeneratedPluginRegistrant.swift
+++ gathering/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,7 +12,6 @@ import firebase_core
 import firebase_storage
 import flutter_secure_storage_macos
 import geolocator_apple
-import path_provider_foundation
 import shared_preferences_foundation
 import sqflite_darwin
 import url_launcher_macos
@@ -26,7 +25,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
